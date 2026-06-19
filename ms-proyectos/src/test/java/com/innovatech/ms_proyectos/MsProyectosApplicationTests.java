7085b255413976492395cff0a68b0fbd31440deb
goCatrilef <go.catrilef@duocuc.cl>
2026-06-19 00:23:17 -0400

    se agregan package y configuraciones

--- ms-proyectos/src/test/java/com/innovatech/ms_proyectos/MsProyectosApplicationTests.java
+++ ms-proyectos/src/test/java/com/innovatech/ms_proyectos/MsProyectosApplicationTests.java
@@ -2,12 +2,13 @@ package com.innovatech.ms_proyectos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MsProyectosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
