e03c0df1ae9286cef3039c0617e3f43593365609
goCatrilef <go.catrilef@duocuc.cl>
2026-06-20 10:00:18 -0400

    primeras configuraciones

---
+++ ms-tareas/src/main/java/com/innovatech/ms_tareas/MsTareasApplication.java
@@ -0,0 +1,13 @@
+package com.innovatech.ms_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTareasApplication.class, args);
+	}
+
+}
---
+++ ms-tareas/src/test/java/com/innovatech/ms_tareas/MsTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovatech.ms_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
